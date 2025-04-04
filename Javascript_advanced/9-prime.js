c6b8bcf622c7ec56a05cf12346b7783b749ce37b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-04-04 12:33:54 -0500

    moved to correct repo

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,14 @@
+function countPrimeNumbers() {
+    let count = 0;
+    let number = 2;
+
+    while (number <= 100) {
+        let prime = true;
+        for (let )
+    } 
+
+const startTime = performance.now();
+const primeCount = countPrimeNumbers();
+const endTime = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${endTime - startTime} milliseconds.`);
